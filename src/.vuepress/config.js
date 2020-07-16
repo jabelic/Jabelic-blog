5fe31e05ef00e53e9ef57b326a0886b1da7b2004
Hibiki Mizuno <hibiki990523@gmail.com>
2020-07-16 23:35:07 +0900

    install plugins

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -6,5 +6,19 @@ module.exports = {
         { text: 'About', link: '/info/info.html' },
         { text: 'Github', link: 'https://github.com/jabelic' }
       ]
-    }
+    },
+    plugins: [
+        [
+          '@vuepress/google-analytics',
+          {
+            'ga': 'UA-148177675-2' // UA-00000000-0, GoogleAnalyticsトラッキングID
+          }
+        ],
+        [
+        'sitemap',
+            {
+            hostname: 'https://jabelic.netlify.app'
+            },
+        ]
+    ]
   }
